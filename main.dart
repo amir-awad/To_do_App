86aa4d44c2a493cdf61769a226a3505e1899917e
Ahmed_Osama <94145850+AHMEDOSAMA50@users.noreply.github.com>
2022-04-05 13:53:33 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/provider/tasks_data_provider.dart';
+
+import 'home_page/ToDo_App.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+    return ChangeNotifierProvider(
+      create: (context) => Tasks_Data_Provider(),
+      child: const MaterialApp(
+        home: ToDo_App(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
